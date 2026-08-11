5b7b49bcb6fb7f3b8ffd69c8ab7a7492406ac0b0
NamPHsk <phamhainam5252@gmail.com>
2026-08-11 15:58:43 +0700

    Port config screen to BoxLib 22 UI API

--- common/src/main/java/dev/boxadactle/macrocraft/config/MacroCraftConfigScreen.java
+++ common/src/main/java/dev/boxadactle/macrocraft/config/MacroCraftConfigScreen.java
@@ -3,85 +3,80 @@ package dev.boxadactle.macrocraft.config;
 import dev.boxadactle.boxlib.gui.config.BOptionScreen;
 import dev.boxadactle.boxlib.gui.config.widget.BSpacingEntry;
 import dev.boxadactle.boxlib.gui.config.widget.button.BBooleanButton;
-import dev.boxadactle.boxlib.gui.config.widget.button.BConfigScreenButton;
+import dev.boxadactle.boxlib.gui.config.widget.button.BScreenButton;
 import dev.boxadactle.boxlib.util.ClientUtils;
 import dev.boxadactle.macrocraft.MacroCraft;
 import dev.boxadactle.macrocraft.gui.MacroListScreen;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class MacroCraftConfigScreen extends BOptionScreen {
 
     public MacroCraftConfigScreen(Screen parent) {
-        super(parent);
-
+        super(parent, Component.translatable("screen.macrocraft.config.title", MacroCraft.VERSION_STRING));
+        this.parent = parent;
         MacroCraft.CONFIG.cacheConfig();
     }
 
     @Override
-    protected Component getName() {
-        return Component.translatable("screen.macrocraft.config.title", MacroCraft.VERSION_STRING);
-    }
-
-    @Override
-    protected void initFooter(int startX, int startY) {
-        this.addRenderableWidget(createHalfCancelButton(startX, startY, b -> {
-            ClientUtils.setScreen(parent);
+    protected void initFooter(LinearLayout layout) {
+        layout.addChild(createCancelButton(b -> {
             MacroCraft.CONFIG.restoreCache();
+            ClientUtils.setScreen(parent);
         }));
 
-        this.setSaveButton(createHalfSaveButton(startX, startY, b -> {
-            ClientUtils.setScreen(parent);
+        setSaveButton(layout.addChild(createSaveButton(b -> {
             MacroCraft.CONFIG.save();
-        }));
+            ClientUtils.setScreen(parent);
+        })));
     }
 
     @Override
-    protected void initConfigButtons() {
-        BBooleanButton b = new BBooleanButton(
+    protected void addOptions() {
+        BBooleanButton blockInputs = new BBooleanButton(
                 "screen.macrocraft.config.blockInputsWhenPlaying",
                 MacroCraft.CONFIG.get().blockInputsWhenPlaying,
-                (value) -> MacroCraft.CONFIG.get().blockInputsWhenPlaying = value
+                value -> MacroCraft.CONFIG.get().blockInputsWhenPlaying = value
         );
-        b.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.blockInputsWhenPlaying.description")));
-        addConfigLine(b);
+        blockInputs.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.blockInputsWhenPlaying.description")));
+        addConfigLine(blockInputs);
 
-        BBooleanButton c = new BBooleanButton(
+        BBooleanButton ignoreMenu = new BBooleanButton(
                 "screen.macrocraft.config.ignoreMenuNavigation",
                 MacroCraft.CONFIG.get().ignoreMenuNavigation,
-                (value) -> MacroCraft.CONFIG.get().ignoreMenuNavigation = value
+                value -> MacroCraft.CONFIG.get().ignoreMenuNavigation = value
         );
-        c.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.ignoreMenuNavigation.description")));
-        addConfigLine(c);
+        ignoreMenu.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.ignoreMenuNavigation.description")));
+        addConfigLine(ignoreMenu);
 
-        BBooleanButton d = new BBooleanButton(
+        BBooleanButton ignoreChat = new BBooleanButton(
                 "screen.macrocraft.config.ignoreChatTyping",
                 MacroCraft.CONFIG.get().ignoreChatTyping,
-                (value) -> MacroCraft.CONFIG.get().ignoreChatTyping = value
+                value -> MacroCraft.CONFIG.get().ignoreChatTyping = value
         );
-        d.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.ignoreChatTyping.description")));
-        addConfigLine(d);
+        ignoreChat.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.ignoreChatTyping.description")));
+        addConfigLine(ignoreChat);
 
-        BBooleanButton e = new BBooleanButton(
+        BBooleanButton moveMouse = new BBooleanButton(
                 "screen.macrocraft.config.moveMouseWhenPlaying",
                 MacroCraft.CONFIG.get().moveMouseWhenPlaying,
-                (value) -> MacroCraft.CONFIG.get().moveMouseWhenPlaying = value
+                value -> MacroCraft.CONFIG.get().moveMouseWhenPlaying = value
         );
-        e.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.moveMouseWhenPlaying.description")));
-        addConfigLine(e);
+        moveMouse.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.moveMouseWhenPlaying.description")));
+        addConfigLine(moveMouse);
 
-        BBooleanButton f = new BBooleanButton(
+        BBooleanButton renderHud = new BBooleanButton(
                 "screen.macrocraft.config.shouldRenderHud",
                 MacroCraft.CONFIG.get().shouldRenderHud,
-                (value) -> MacroCraft.CONFIG.get().shouldRenderHud = value
+                value -> MacroCraft.CONFIG.get().shouldRenderHud = value
         );
-        e.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.shouldRenderHud.description")));
-        addConfigLine(f);
+        renderHud.setTooltip(Tooltip.create(Component.translatable("screen.macrocraft.config.shouldRenderHud.description")));
+        addConfigLine(renderHud);
 
         addConfigLine(new BSpacingEntry());
-
-        addConfigLine(new BConfigScreenButton(
+        addConfigLine(new BScreenButton(
                 Component.translatable("screen.macrocraft.config.macroManager"),
                 this,
                 MacroListScreen::new
