e528564595cfdf8b6a90d91a7a20c0bc8a3724df
NamPHsk <phamhainam5252@gmail.com>
2026-08-11 15:49:36 +0700

    Port MouseHandler invoker to 26.2 API

--- common/src/main/java/dev/boxadactle/macrocraft/listeners/MouseInvoker.java
+++ common/src/main/java/dev/boxadactle/macrocraft/listeners/MouseInvoker.java
@@ -1,19 +1,19 @@
 package dev.boxadactle.macrocraft.listeners;
 
 import net.minecraft.client.MouseHandler;
+import net.minecraft.client.input.MouseButtonInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(MouseHandler.class)
 public interface MouseInvoker {
 
-    @Invoker("onPress")
-    void invokeMousePress(long l, int i, int j, int k);
+    @Invoker("onButton")
+    void invokeMouseButton(long window, MouseButtonInfo buttonInfo, int action);
 
     @Invoker("onMove")
-    void invokeMove(long l, double d, double e);
+    void invokeMove(long window, double x, double y);
 
     @Invoker("onScroll")
-    void invokeScroll(long l, double d, double e);
-
+    void invokeScroll(long window, double xOffset, double yOffset);
 }
