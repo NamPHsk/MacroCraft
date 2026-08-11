7168abab8033de71dec0fcd7767779dc5cf11bcb
NamPHsk <phamhainam5252@gmail.com>
2026-08-11 15:47:30 +0700

    Port key mappings to Minecraft 26.2 API

--- common/src/main/java/dev/boxadactle/macrocraft/MacroCraftKeybinds.java
+++ common/src/main/java/dev/boxadactle/macrocraft/MacroCraftKeybinds.java
@@ -1,40 +1,44 @@
 package dev.boxadactle.macrocraft;
 
-import dev.boxadactle.boxlib.function.EmptyMethod;
-import dev.boxadactle.macrocraft.listeners.KeyAccessor;
+import com.mojang.blaze3d.platform.InputConstants;
 import dev.boxadactle.macrocraft.macro.MacroState;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class MacroCraftKeybinds {
 
-    public static KeyMapping hideGui = new KeyMapping("key.macrocraft.hide_gui", GLFW.GLFW_KEY_X, "key.categories.macrocraft");
+    private static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+            Identifier.fromNamespaceAndPath(MacroCraft.MOD_ID, "macrocraft")
+    );
 
-    public static KeyMapping playMacro = new KeyMapping("key.macrocraft.play_macro", GLFW.GLFW_KEY_K, "key.categories.macrocraft");
+    public static final KeyMapping hideGui = new KeyMapping("key.macrocraft.hide_gui", GLFW.GLFW_KEY_X, CATEGORY);
+    public static final KeyMapping playMacro = new KeyMapping("key.macrocraft.play_macro", GLFW.GLFW_KEY_K, CATEGORY);
+    public static final KeyMapping pauseMacro = new KeyMapping("key.macrocraft.pause_macro", GLFW.GLFW_KEY_P, CATEGORY);
+    public static final KeyMapping stopMacro = new KeyMapping("key.macrocraft.stop_macro", GLFW.GLFW_KEY_L, CATEGORY);
+    public static final KeyMapping openMacroList = new KeyMapping("key.macrocraft.open_macro_list", GLFW.GLFW_KEY_C, CATEGORY);
 
-    public static KeyMapping pauseMacro = new KeyMapping("key.macrocraft.pause_macro", GLFW.GLFW_KEY_P, "key.categories.macrocraft");
+    private static boolean matches(KeyMapping mapping, int code) {
+        return mapping.matches(InputConstants.Type.KEYSYM.getOrCreate(code));
+    }
 
-    public static KeyMapping stopMacro = new KeyMapping("key.macrocraft.stop_macro", GLFW.GLFW_KEY_L, "key.categories.macrocraft");
-
-    public static KeyMapping openMacroList = new KeyMapping("key.macrocraft.open_macro_list", GLFW.GLFW_KEY_C, "key.categories.macrocraft");
-
-    public static void checkKeybind(int code, EmptyMethod resume, EmptyMethod pause, EmptyMethod stop) {
-        if (((KeyAccessor)hideGui).getKey().getValue() == code) {
+    public static void checkKeybind(int code, Runnable resume, Runnable pause, Runnable stop) {
+        if (matches(hideGui, code)) {
             MacroCraft.CONFIG.get().shouldRenderHud = !MacroCraft.CONFIG.get().shouldRenderHud;
             MacroCraft.CONFIG.save();
-        } else if (((KeyAccessor)playMacro).getKey().getValue()  == code) {
-            resume.accept();
-        } else if (((KeyAccessor)pauseMacro).getKey().getValue()  == code) {
-            pause.accept();
-        } else if (((KeyAccessor)stopMacro).getKey().getValue()  == code) {
-            stop.accept();
+        } else if (matches(playMacro, code)) {
+            resume.run();
+        } else if (matches(pauseMacro, code)) {
+            pause.run();
+        } else if (matches(stopMacro, code)) {
+            stop.run();
         }
     }
 
     public static boolean shouldIgnoreInput(int code) {
-        return ((KeyAccessor)hideGui).getKey().getValue() == code
-                || ((KeyAccessor)playMacro).getKey().getValue() == code
-                || ((KeyAccessor)pauseMacro).getKey().getValue() == code
-                || ((KeyAccessor)stopMacro).getKey().getValue() == code;
+        return matches(hideGui, code)
+                || matches(playMacro, code)
+                || matches(pauseMacro, code)
+                || matches(stopMacro, code);
     }
 }
