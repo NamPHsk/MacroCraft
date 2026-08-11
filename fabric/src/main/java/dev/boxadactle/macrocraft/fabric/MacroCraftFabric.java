30d946fc8a3735fbd01072f1eda703bac6aecd50
NamPHsk <phamhainam5252@gmail.com>
2026-08-11 16:14:57 +0700

    Port Fabric client entrypoint and HUD registry to 26.2

--- fabric/src/main/java/dev/boxadactle/macrocraft/fabric/MacroCraftFabric.java
+++ fabric/src/main/java/dev/boxadactle/macrocraft/fabric/MacroCraftFabric.java
@@ -1,34 +1,32 @@
 package dev.boxadactle.macrocraft.fabric;
 
-import dev.boxadactle.boxlib.fabric.command.BCommandManager;
+import dev.boxadactle.boxlib.util.ClientUtils;
 import dev.boxadactle.macrocraft.MacroCraft;
 import dev.boxadactle.macrocraft.MacroCraftKeybinds;
-import dev.boxadactle.macrocraft.fabric.command.ConfigCommand;
-import dev.boxadactle.macrocraft.fabric.command.PlayCommand;
-import dev.boxadactle.macrocraft.fabric.command.RecordCommand;
-import dev.boxadactle.macrocraft.fabric.command.SaveCommand;
 import dev.boxadactle.macrocraft.gui.MacroListScreen;
 import dev.boxadactle.macrocraft.hud.MacroPlayHud;
 import dev.boxadactle.macrocraft.hud.MacroRecordHud;
 import dev.boxadactle.macrocraft.macro.MacroState;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.resources.Identifier;
 
-public class MacroCraftFabric implements ModInitializer {
+public class MacroCraftFabric implements ClientModInitializer {
+    private static final Identifier HUD_ELEMENT = Identifier.fromNamespaceAndPath(MacroCraft.MOD_ID, "macro_hud");
+
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         MacroCraft.init();
 
         ClientTickEvents.END_CLIENT_TICK.register(this::tick);
-
-        HudRenderCallback.EVENT.register(this::renderMacroHud);
+        HudElementRegistry.attachElementBefore(VanillaHudElements.CHAT, HUD_ELEMENT, this::extractMacroHud);
 
         initKeybinds();
-        initCommands();
     }
 
     private void initKeybinds() {
@@ -39,24 +37,15 @@ public class MacroCraftFabric implements ModInitializer {
         KeyBindingHelper.registerKeyBinding(MacroCraftKeybinds.openMacroList);
     }
 
-    private void initCommands() {
-        BCommandManager.registerCommand("macro", (list) -> {
-            list.add(ConfigCommand::new);
-            list.add(PlayCommand::new);
-            list.add(RecordCommand::new);
-            list.add(SaveCommand::new);
-        });
-    }
-
     private void tick(Minecraft client) {
         MacroState.tick();
 
         if (MacroCraftKeybinds.openMacroList.consumeClick()) {
-            client.setScreen(new MacroListScreen(null));
+            ClientUtils.setScreen(new MacroListScreen(null));
         }
     }
 
-    private void renderMacroHud(GuiGraphics graphics, float tickDelta) {
+    private void extractMacroHud(GuiGraphicsExtractor graphics, net.minecraft.client.DeltaTracker deltaTracker) {
         if (MacroState.LOADED_MACRO.shouldRenderHud()) {
             MacroPlayHud.render(graphics);
         }
