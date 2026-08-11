9a1bfebc3c2a15d1069aef7f79aa32945229d6fe
NamPHsk <phamhainam5252@gmail.com>
2026-08-11 15:50:36 +0700

    Guard replayed mouse scrolling on 26.2

--- common/src/main/java/dev/boxadactle/macrocraft/macro/action/MouseScrollAction.java
+++ common/src/main/java/dev/boxadactle/macrocraft/macro/action/MouseScrollAction.java
@@ -2,9 +2,9 @@ package dev.boxadactle.macrocraft.macro.action;
 
 import com.google.gson.JsonObject;
 import dev.boxadactle.boxlib.util.ClientUtils;
-import dev.boxadactle.macrocraft.MacroCraft;
-import dev.boxadactle.macrocraft.macro.MacroAction;
 import dev.boxadactle.macrocraft.listeners.MouseInvoker;
+import dev.boxadactle.macrocraft.macro.MacroAction;
+import dev.boxadactle.macrocraft.macro.MacroState;
 import net.minecraft.client.MouseHandler;
 
 public class MouseScrollAction extends MacroAction {
@@ -13,17 +13,21 @@ public class MouseScrollAction extends MacroAction {
 
     public MouseScrollAction(int startTicks, double xoffset, double yoffset) {
         super(startTicks);
-
         this.xoffset = xoffset;
         this.yoffset = yoffset;
     }
 
     @Override
     public void execute() {
-        MouseHandler m = ClientUtils.getClient().mouseHandler;
+        MouseHandler handler = ClientUtils.getClient().mouseHandler;
         long window = ClientUtils.getWindow();
 
-        ((MouseInvoker) m).invokeScroll(window, xoffset, yoffset);
+        MacroState.IS_REPLAYING_INPUT = true;
+        try {
+            ((MouseInvoker) handler).invokeScroll(window, xoffset, yoffset);
+        } finally {
+            MacroState.IS_REPLAYING_INPUT = false;
+        }
     }
 
     @Override
@@ -31,7 +35,6 @@ public class MouseScrollAction extends MacroAction {
         JsonObject dataObject = new JsonObject();
         dataObject.addProperty("xoffset", xoffset);
         dataObject.addProperty("yoffset", yoffset);
-
         return dataObject;
     }
 
